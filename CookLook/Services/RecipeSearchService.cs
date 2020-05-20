5d3c7776ed8c9ef451f4d58b8f2a91d28322c16f
MarkLowis <mark.lowis@hotmail.com>
2020-05-20 11:32:16 +0100

    Search bar and code behind classes added

---
+++ CookLook/Services/RecipeSearchService.cs
@@ -0,0 +1,16 @@
+﻿using CookLook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookLook.Services
+{
+    public class RecipeSearchService : IRecipeSearchService
+    {
+        public async Task<List<Recipe>> SearchRecipesAsync(string searchterm)
+        {
+            return new List<Recipe> { new Recipe { Label = "Cockroach Hotdog", Url = "www.cockroachhotdog.com" } };
+        }
+    }
+}
